ed6faffca60c4d2a95ff383b14149e057c9a7e01
drunkpanda <open0102@gmail.com>
2013-09-12 11:07:50 +0800

    Create class_template.js

---
+++ class_template.js
@@ -0,0 +1,38 @@
+/**
+ * Javascript class boilerplate (Literal version). 
+ * required jQuery for $.extend to work. 
+ * last update: 2013/09/12
+ */
+var class_template = {
+    type: 'category',
+    // act as constructor
+    init:function( options ){
+
+         // Get instance
+         var self = class_template;
+        
+         // Setup default settings.
+         this.cfg = $.extend({
+            status_show_info: true         
+         }, options );
+        
+         self.info('Initializing');
+         
+         // Start bind_events()
+         self.bind_events();
+    },
+    // to hold functions 
+    bind_events:function(){
+         // Get instance
+         var self = class_template;
+
+         self.info('bind_events(): started');
+    },
+    // Display message 
+    info:function( message ){
+          if(this.cfg.status_show_info == true){
+             console.info(message);   
+          }
+     }       
+};
+class_template.init();
